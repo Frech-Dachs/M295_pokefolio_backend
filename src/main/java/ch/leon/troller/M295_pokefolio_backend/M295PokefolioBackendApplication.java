71e4574bf329727acc5ec9aee0a8ca33c4afa7f7
Leon <leontroller@icloud.com>
2026-05-04 13:51:00 +0200

    init

---
+++ src/main/java/ch/leon/troller/M295_pokefolio_backend/M295PokefolioBackendApplication.java
@@ -0,0 +1,13 @@
+package ch.leon.troller.M295_pokefolio_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M295PokefolioBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M295PokefolioBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/leon/troller/M295_pokefolio_backend/M295PokefolioBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.leon.troller.M295_pokefolio_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M295PokefolioBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
